f4041d7f83eafb665fe491663598850dc7fd84e7
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-04-28 13:34:16 -0400

    write smoke and snapshot for carousel

--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -1,19 +1,30 @@
-import React from "react";
-import { render, fireEvent } from "@testing-library/react";
-import Carousel from "./Carousel";
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Carousel from './Carousel';
 
-it("works when you click on the right arrow", function() {
-  const { queryByTestId, queryByAltText } = render(<Carousel />);
+it('works when you click on the right arrow', function() {
+	const { queryByTestId, queryByAltText } = render(<Carousel />);
 
-  // expect the first image to show, but not the second
-  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
-  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
+	// expect the first image to show, but not the second
+	expect(queryByAltText('Photo by Richard Pasquarella on Unsplash')).toBeInTheDocument();
+	expect(queryByAltText('Photo by Pratik Patel on Unsplash')).not.toBeInTheDocument();
 
-  // move forward in the carousel
-  const rightArrow = queryByTestId("right-arrow");
-  fireEvent.click(rightArrow);
+	// move forward in the carousel
+	const rightArrow = queryByTestId('right-arrow');
+	fireEvent.click(rightArrow);
 
-  // expect the second image to show, but not the first
-  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).not.toBeInTheDocument();
-  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
+	// expect the second image to show, but not the first
+	expect(queryByAltText('Photo by Richard Pasquarella on Unsplash')).not.toBeInTheDocument();
+	expect(queryByAltText('Photo by Pratik Patel on Unsplash')).toBeInTheDocument();
+});
+
+// Smoke
+test('It renders without crashing', () => {
+	render(<Carousel />);
+});
+
+// Snapshot
+it('matches snapshot', function() {
+	const { asFragment } = render(<Carousel title="Test Title" />);
+	expect(asFragment()).toMatchSnapshot();
 });
